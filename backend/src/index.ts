ecfba4c82723ce801a25ed89e4d6f4609a232ba5
Jonathan Scattolini <jscattolini@gmail.com>
2023-09-25 21:53:08 +0200

    :recycle: Backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,37 +24,59 @@ app.use(express.json());
 const port = 5000;
 
 //-----------------------------------------
+//-----------TRY CATCH ERRORS--------------
+//-----------------------------------------
+
+function asyncController(controller: Function) {
+  return async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    try {
+      await controller(req, res, next);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send();
+    }
+  };
+}
+
+//-----------------------------------------
 //-----------------ADS---------------------
 //-----------------------------------------
 
 const adsController = new AdsController();
-app.get("/api/ads", adsController.getAll);
-app.get("/api/ads/:id", adsController.getOne);
-app.post("/api/ads", adsController.createOne);
-app.delete("/api/ads/:id", adsController.deleteOne);
-app.patch("/api/ads/:id", adsController.patchOne);
+app.get("/api/ads", asyncController(adsController.getAll));
+app.get("/api/ads/:id", asyncController(adsController.getOne));
+app.post("/api/ads", asyncController(adsController.createOne));
+app.delete("/api/ads/:id", asyncController(adsController.deleteOne));
+app.patch("/api/ads/:id", asyncController(adsController.patchOne));
 
 //-----------------------------------------
 //-----------------CATEGORY----------------
 //-----------------------------------------
 
 const categoriesController = new CategoriesController();
-app.get("/api/category", categoriesController.getAll);
-app.get("/api/category/:id", categoriesController.getOne);
+app.get("/api/category", asyncController(categoriesController.getAll));
+app.get("/api/category/:id", asyncController(categoriesController.getOne));
 app.post("/api/category", categoriesController.createOne);
-app.delete("/api/category/:id", categoriesController.deleteOne);
-app.patch("/api/category/:id", categoriesController.patchOne);
+app.delete(
+  "/api/category/:id",
+  asyncController(categoriesController.deleteOne)
+);
+app.patch("/api/category/:id", asyncController(categoriesController.patchOne));
 
 //-----------------------------------------
 //-----------------TAG---------------------
 //-----------------------------------------
 
 const tagsController = new TagsController();
-app.get("/api/tag", tagsController.getAll);
-app.get("/api/tag/:id", tagsController.getOne);
+app.get("/api/tag", asyncController(tagsController.getAll));
+app.get("/api/tag/:id", asyncController(tagsController.getOne));
 app.post("/api/tag", tagsController.createOne);
-app.delete("/api/tag/:id", tagsController.deleteOne);
-app.patch("/api/tag/:id", tagsController.patchOne);
+app.delete("/api/tag/:id", asyncController(tagsController.deleteOne));
+app.patch("/api/tag/:id", asyncController(tagsController.patchOne));
 
 //-----------------------------------------
 //-------------SERVER LISTENING------------
