c925286cf3d98b8aba6f8beaac08aab24d29be54
Mega Krash <jscattolini@gmail.com>
2024-01-15 23:02:25 +0100

    :construction: :lipstick: AppBar

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,44 +1,217 @@
 import Link from "next/link";
 import IconButton from "@mui/material/IconButton";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
-import { useMutation } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import { mutationSignOut, queryMeContext } from "../graphql/Users";
+import { UserContextTypes } from "@/types/UserTypes";
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import AdbIcon from "@mui/icons-material/Adb";
+import MenuIcon from "@mui/icons-material/Menu";
+import React from "react";
+
+const pages = ["Products", "Pricing", "Blog"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 export default function Header(): React.ReactNode {
+  // User connected ?
+  const { data } = useQuery<{ items: UserContextTypes }>(queryMeContext);
+  const userConnected = data ? data.items : [];
+
+  // Logout
   const [doSignout] = useMutation(mutationSignOut, {
     refetchQueries: [queryMeContext],
   });
   async function logout() {
     doSignout();
   }
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
 
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
   return (
-    <header className="header">
-      <div className="main-menu">
-        <h1>
-          <Link href="/" className="button logo link-button">
-            <span className="desktop-long-label">THE GOOD CORNER</span>
+    <AppBar position="static" sx={{ backgroundColor: "#343a40" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Link href="/">
+            <AdbIcon
+              sx={{
+                display: { xs: "none", md: "flex" },
+                mr: 1,
+                color: "#e89116",
+              }}
+            />
           </Link>
-        </h1>
-        <Link href="/annonces/new" className="button link-button">
-          <span className="desktop-long-label">Publier une annonce</span>
-        </Link>
-        <Link href="/inscription" className="button link-button">
-          <span className="desktop-long-label">Mon compte</span>
-        </Link>
-        <Link href="/inscription/creation" className="button link-button">
-          <span className="desktop-long-label">Créer un compte</span>
-        </Link>
-        <Link href="/connexion" className="button link-button">
-          <span className="desktop-long-label">Connexion</span>
-        </Link>
-        <Link href="/contact" className="button link-button">
-          <span className="desktop-long-label">Contact</span>
-        </Link>
-        <IconButton onClick={logout} color="primary" aria-label="Logout">
-          <ExitToAppIcon fontSize="inherit" />
-        </IconButton>
-      </div>
-    </header>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              // fontFamily: "monospace",
+              fontWeight: 700,
+              // letterSpacing: ".3rem",
+              color: "#e89116",
+              textDecoration: "none",
+            }}
+          >
+            THE GOOD CORNER
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            TGC
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    // <header className="header">
+    //   <div className="main-menu">
+    //     <h1>
+    //       <Link href="/" className="button logo link-button">
+    //         <span className="desktop-long-label">THE GOOD CORNER</span>
+    //       </Link>
+    //     </h1>
+    //     <Link href="/annonces/new" className="button link-button">
+    //       <span className="desktop-long-label">Publier une annonce</span>
+    //     </Link>
+    //     <Link href="/inscription" className="button link-button">
+    //       <span className="desktop-long-label">Mon compte</span>
+    //     </Link>
+    //     <Link href="/inscription/creation" className="button link-button">
+    //       <span className="desktop-long-label">Créer un compte</span>
+    //     </Link>
+    //     <Link href="/connexion" className="button link-button">
+    //       <span className="desktop-long-label">Connexion</span>
+    //     </Link>
+    //     <Link href="/contact" className="button link-button">
+    //       <span className="desktop-long-label">Contact</span>
+    //     </Link>
+    //     <IconButton onClick={logout} color="primary" aria-label="Logout">
+    //       <ExitToAppIcon fontSize="inherit" />
+    //     </IconButton>
+    //   </div>
+    // </header>
   );
 }
