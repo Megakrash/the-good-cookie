3d9aadb33c5eca85e125118389ff2f2170a00bdc
Mega Krash <jscattolini@gmail.com>
2024-03-21 18:03:10 +0100

    nouveau test ci avec tag

--- frontend/src/components/TestCi.tsx
+++ frontend/src/components/TestCi.tsx
@@ -4,7 +4,7 @@ const TestCi = () => {
   return (
     <div>
       Test CI again and again tin de sa mère. Maintenant on build et push avec
-      le tag
+      le tag et on va voir si ça marche.
     </div>
   );
 };
