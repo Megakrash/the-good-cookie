5b87828bfc619530984cdb433e8cb16216607eae
Mega Krash <jscattolini@gmail.com>
2024-06-23 10:22:11 +0200

    :wrench:

--- frontend/src/api/configApi.ts
+++ frontend/src/api/configApi.ts
@@ -6,9 +6,7 @@ export const MIDDLEWARE_URL =
     ? "http://localhost:5000/api/"
     : "/api/";
 export const PATH_IMAGE =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:5005/pictures"
-    : "/pictures";
+  process.env.PATH_IMAGE || "http://localhost:5005/pictures/";
 export const RECAPTCHA_SITE_KEY: string =
   process.env.NODE_ENV === "development"
     ? "6LctKSQpAAAAAOdHh-YB8K9XDvf93Qeko1r5nfRl"
