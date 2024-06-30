d025579c0e5f810a60f6469d5295713547b7da42
Mega Krash <jscattolini@gmail.com>
2024-06-30 10:59:54 +0200

    :wrench: test no preflight appolo client

--- frontend/src/config/apollo-client.ts
+++ frontend/src/config/apollo-client.ts
@@ -7,9 +7,6 @@ import { API_URL, WS_URL } from "../api/configApi";
 const httpLink = new HttpLink({
   uri: API_URL,
   credentials: "include",
-  headers: {
-    "apollo-require-preflight": "true",
-  },
 });
 
 const wsLink = new GraphQLWsLink(
